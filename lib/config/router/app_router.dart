a1411f6807c124a16a1a71f7e65130990cecc22d
nachito02 <arguellojuan08@gmail.com>
2026-01-21 04:13:39 -0300

    Revert "go router official"
    This reverts commit fff82618b59947dc73f38cffa00839d1491eecc1.

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -1,71 +1,25 @@
 import 'package:cinemapedia/presentation/screens/screens.dart';
-import 'package:cinemapedia/presentation/views/views.dart';
 import 'package:go_router/go_router.dart';
 
 final appRputer = GoRouter(
   initialLocation: '/',
 
   routes: [
-    ShellRoute(
-      builder: (context, state, child) {
-        return HomeScreen(childView: child);
-      },
+    GoRoute(
+      path: '/',
+      name: HomeScreen.name,
+      builder: (context, state) => const HomeScreen(),
+
       routes: [
         GoRoute(
-          path: '/',
+          path: 'movie/:id',
+          name: MovieScreen.name,
           builder: (context, state) {
-            return const HomeView();
-          },
-
-          routes: [
-            GoRoute(
-              path: '/',
-              name: HomeScreen.name,
-              builder: (context, state) =>
-                  const HomeScreen(childView: HomeView()),
-
-              routes: [
-                GoRoute(
-                  path: 'movie/:id',
-                  name: MovieScreen.name,
-                  builder: (context, state) {
-                    final movieId = state.pathParameters['id'] ?? 'no-id';
-                    return MovieScreen(movieId: movieId);
-                  },
-                ),
-              ],
-            ),
-          ],
-        ),
-
-        GoRoute(
-          path: '/favorites',
-          builder: (context, state) {
-            return const FavoritesView();
+            final movieId = state.pathParameters['id'] ?? 'no-id';
+            return MovieScreen(movieId: movieId);
           },
         ),
       ],
     ),
   ],
-
-  //Rutas padres e hijos
-
-  // routes: [
-  //   GoRoute(
-  //     path: '/',
-  //     name: HomeScreen.name,
-  //     builder: (context, state) => const HomeScreen(childView: HomeView(),),
-
-  //     routes: [
-  //       GoRoute(
-  //         path: 'movie/:id',
-  //         name: MovieScreen.name,
-  //         builder: (context, state) {
-  //           final movieId = state.pathParameters['id'] ?? 'no-id';
-  //           return MovieScreen(movieId: movieId);
-  //         },
-  //       ),
-  //     ],
-  //   ),
-  // ],
 );
--- lib/presentation/widgets/shared/custom_bottom_navigation.dart
+++ lib/presentation/widgets/shared/custom_bottom_navigation.dart
@@ -1,47 +1,14 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 
 class CustomBottomNavigation extends StatelessWidget {
-  int getCurrentIndex(BuildContext context) {
-    final String location = GoRouterState.of(context).uri.toString();
-
-    switch (location) {
-      case '/':
-        return 0;
-
-      case '/categories':
-        return 1;
-
-      case '/favorites':
-        return 2;
-
-      default:
-        return 0;
-    }
-  }
-
-  void onItemTapped(BuildContext context, int index) {
-    switch (index) {
-      case 0:
-        context.go('/');
-      case 1:
-        context.go('/');
-      case 2:
-        context.go('/favorites');
-    }
-  }
-
   const CustomBottomNavigation({super.key});
 
   @override
   Widget build(BuildContext context) {
     return BottomNavigationBar(
       elevation: 0,
-      currentIndex: getCurrentIndex(context),
-      onTap: (value) {
-        onItemTapped(context, value);
-      },
       items: [
+      
         BottomNavigationBarItem(icon: Icon(Icons.home_max), label: 'Inicio'),
         BottomNavigationBarItem(
           icon: Icon(Icons.label_outline),
